a1cb6be5f4c3fe8e97ae2fb9a7f9051472155ff6
unknown <esra.alhussain.s@gmail.com>
2019-07-07 17:58:30 -0300

    first

---
+++ app/src/main/java/com/example/stepbooster_11/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.stepbooster_11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this,"Firebase connection Success",Toast.LENGTH_LONG).show();
+    }
+}
